c57c8416a71b1185183984f8dc60c857a7e0e3fb
zrb <mrzhangrb@outlook.com>
2018-05-20 21:53:12 +0800

    init

---
+++ src/common/js/config.js
@@ -0,0 +1,21 @@
+export const playMode = {
+  sequence: 0,
+  loop: 1,
+  random: 2
+}
+
+export const commonParams = {
+  g_kt: 5381,
+  uin: 0,
+  notice: 0,
+  format: 'json',
+  inCharset: 'utf-8',
+  outCharset: 'utf-8',
+  platform: 'h5'
+}
+
+export const options = {
+  param: 'jsonpCallback'
+}
+
+export const ERR_OK = 0
